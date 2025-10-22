1ec4f1033bd2c44227de9f0bd4d2efbb3f9d1928
inno-cent <ekweozorinnocent@gmail.com>
2025-10-22 19:09:14 +0100

    pages: update on setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <RouterView />
+  <div class="flex h-screen bg-gray-100">
+    <Sidebar />
+    <div class="flex-1 flex flex-col">
+      <Navbar />
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router'
+<script>
+import Sidebar from './components/layout/Sidebar.vue'
+import Navbar from './components/layout/Navbar.vue'
+
+export default {
+  components: { Sidebar, Navbar }
+}
 </script>
