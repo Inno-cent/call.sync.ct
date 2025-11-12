3c50facda7bba456054189107cb352bb7adfb4ac
inno-cent <ekweozorinnocent@gmail.com>
2025-11-12 23:18:30 +0100

    tim

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex min-h-screen bg-[linear-gradient(180deg,#071026,rgba(10,12,16,0.3))] text-white"
+  >
+    <Sidebar />
+    <div class="flex-1 flex flex-col">
+      <Navbar @toggleSidebar="toggleSidebar" />
+      <main class="p-6 overflow-auto">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Sidebar from "@/components/layouts/Sidebar.vue";
+import Navbar from "@/components/layouts/Navbar.vue";
+export default {
+  components: { Sidebar, Navbar },
+  methods: {
+    toggleSidebar() {
+      /* optional */
+    },
+  },
+};
+</script>
